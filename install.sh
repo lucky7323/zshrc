57eef524e69f0bd7d1157b983fbf7ecb0fbba387
eunho-lee <eunho.ambition@gmail.com>
2021-04-13 19:00:07 +0900

    fix: separte install scripts

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ cd ~/.zsh
 git submodule update --init zplug
 git submodule update --remote zplug
 
-[ ! -d ~/.oh-my-zsh/ ] && \
-  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-
 [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k ] && \
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
---
+++ oh-my-zsh.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+[ ! -d ~/.oh-my-zsh/ ] && \
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
