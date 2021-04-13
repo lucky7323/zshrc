98e0648453e6a0f16e63d87693f0df63529425c3
eunho-lee <eunho.ambition@gmail.com>
2021-04-13 17:41:20 +0900

    add oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 
-if [ -f ~/.zshrc ] ; then
+if [ ! -d ~/.oh-my-zsh/ ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k}
+fi
+
+if [ -f ~/.zshrc ]; then
   rm ~/.zshrc
 fi
 ln -s ~/.zsh/zshrc ~/.zshrc
